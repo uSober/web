aeef55760c62340fced3fa303c0bb35da2aec57e
NickLewanowicz <nicklewanowicz@gmail.com>
2018-11-03 00:07:06 -0400

    Add example camera view finder

--- src/sections/Home/components/SelfieClassifier/SelfieClassifier.jsx
+++ src/sections/Home/components/SelfieClassifier/SelfieClassifier.jsx
@@ -1,14 +1,35 @@
 import * as React from 'react';
 import './SelfieClassifier.css'
+import Camera from 'react-html5-camera-photo';
+import { Button } from 'antd';
+
+import 'react-html5-camera-photo/build/css/index.css';
 
 export class SelfieClassifier extends React.Component {
+  state = {
+    cameraOn: false
+  }
   render() {
+    const {cameraOn} = this.state
+    const camera = cameraOn ? 
+      <Camera onTakePhoto = { (dataUri) => { this.onTakePhoto(dataUri) } }/> : 
+      <Button type="primary" shape="circle" icon="camera" size='large' onClick={this.toggleCamera}/>
     return (
       <div>
         This is the SelfieClassifier
+        <div>{camera}</div>
       </div>
     )
   }
+  toggleCamera = () => {
+   const {cameraOn} = this.state
+   this.setState({cameraOn: !cameraOn}) 
+  }
+  onTakePhoto = (dataUri) => {
+    this.toggleCamera()
+    // Do stuff with the dataUri photo...
+  }
+
 }
 
 export default SelfieClassifier
