ad7825520b363337ab827487b190919ae1dab616
NickLewanowicz <nicklewanowicz@gmail.com>
2018-11-03 09:22:38 -0400

    Init balance test steps

--- src/sections/Home/components/BalanceTest/BalanceTest.jsx
+++ src/sections/Home/components/BalanceTest/BalanceTest.jsx
@@ -4,11 +4,16 @@ import './BalanceTest.css'
 export class BalanceTest extends React.Component {
   render() {
     return (
-      <>
-      <div className='header'>
-        <h1>One Leg Balance Test</h1>
+      <div className='stack'>
+        <h1><em>Balance</em></h1>
+        <div className='instructions'>
+        <p className='instruction'>This will measure your balance.</p>
+        <p className='instruction'>Step 1: Stand on your left leg.</p>
+        <p className='instruction'>Step 2: Press start button.</p>
+        <p className='instruction'>Step 3: Extend arms out to the side.</p>
+        <p className='instruction'>Step 4: Wait for completion sound.</p>
+        </div>
       </div>
-      </>
     )
   }
 }
