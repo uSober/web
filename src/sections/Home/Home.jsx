ec5c0d0c0f434e64dd6f3dc06baa4c09af3b045d
NickLewanowicz <nicklewanowicz@gmail.com>
2018-11-02 23:14:07 -0400

    Add conditional rendering for home

--- src/sections/Home/Home.jsx
+++ src/sections/Home/Home.jsx
@@ -4,14 +4,31 @@ import './Home.css'
 import {ReactionTime, SelfieClassifier, Survey} from './components'
 
 export class Home extends React.Component {
+  state = {
+    step: 1
+  }
   render() {
+    const {step} = this.state
+    let curr = null
+    switch(step){
+      case 1:
+        curr = SelfieClassifier
+        break
+      case 2:
+        curr = ReactionTime
+        break
+      case 3:
+        curr = Survey
+        break
+      default:
+        curr = null
+    }
+
     return (
       <div className='mainContainer'>
         <div className='location'>
           This is the Home
-          <ReactionTime/>
-          <SelfieClassifier/>
-          <Survey/>
+          {curr}
         </div>
         
       </div>
