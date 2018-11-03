6d2635995dddc4af0f9b1e6a473bd22842e74e64
NickLewanowicz <nicklewanowicz@gmail.com>
2018-11-02 23:31:07 -0400

    add buttons for example

--- src/sections/Home/Home.jsx
+++ src/sections/Home/Home.jsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { Button, Icon } from 'antd';
 import './Home.css'
 
 import {ReactionTime, SelfieClassifier, Survey} from './components'
@@ -12,13 +13,13 @@ export class Home extends React.Component {
     let curr = null
     switch(step){
       case 1:
-        curr = SelfieClassifier
+        curr = <SelfieClassifier/>
         break
       case 2:
-        curr = ReactionTime
+        curr = <ReactionTime/>
         break
       case 3:
-        curr = Survey
+        curr = <Survey/>
         break
       default:
         curr = null
@@ -29,10 +30,27 @@ export class Home extends React.Component {
         <div className='location'>
           This is the Home
           {curr}
+          <div>
+          <Button.Group size='small'>
+          <Button type="primary" onClick={this.prevStep}>
+              <Icon type="left" />Backward
+            </Button>
+            <Button type="primary" onClick={this.nextStep}>
+              Forward<Icon type="right" />
+            </Button>
+          </Button.Group>
+          </div>
         </div>
-        
       </div>
     )
+  }
+  nextStep = () => {
+    const step = this.state.step%3 + 1
+    this.setState({step})
+  }
+  prevStep = () => {
+    const step = Math.max(1, this.state.step - 1)
+    this.setState({step})
   }
 }
 
