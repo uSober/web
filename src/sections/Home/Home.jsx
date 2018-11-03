3c11e72d5c3de26b902d352963704f9f3e1f822b
NickLewanowicz <nicklewanowicz@gmail.com>
2018-11-02 22:05:21 -0400

    SKeleton Home component

---
+++ src/sections/Home/Home.jsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import './Home.css'
+
+export class Home extends React.Component {
+  render() {
+    return (
+      <div>
+        This is the Home
+      </div>
+    )
+  }
+}
+
+export default Home
