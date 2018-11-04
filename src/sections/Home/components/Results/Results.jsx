e8b8468e2e5c69e8ffd8e9b14c68b855b8547805
NickLewanowicz <nicklewanowicz@gmail.com>
2018-11-03 21:54:23 -0400

    App wont crash if skipped steps

--- src/sections/Home/components/Results/Results.jsx
+++ src/sections/Home/components/Results/Results.jsx
@@ -6,10 +6,10 @@ export class Results extends React.Component {
     reaction: null
   }
   render() {
+    this.getResults()
     return (
       <div className='stack'>
         <h1><em>Results</em></h1>
-        {!this.state.reaction && this.getResults()}
         {this.state.reaction}
         <div className='instructions'>
         </div>
@@ -21,17 +21,25 @@ export class Results extends React.Component {
     const reactionAvg = 400
     const reactionSD = 40
     const maxDeviation = 3
-    const {balanceData, reactionData, selfieData} = this.props;
-    console.log(balanceData, reactionData, selfieData);
-    const myReactionMean = reactionData.reduce((a, b) => { return a + b; })/reactionData.length
-    console.log(myReactionMean, ((myReactionMean - reactionAvg)/reactionSD)/maxDeviation*100)
-    this.setState({reaction: ((myReactionMean - reactionAvg)/reactionSD)/maxDeviation*100})
-    const myBalanceMean = balanceData.reduce((a, b) => { return {gamma: a.gamma + b.gamma, beta: a.beta + b.beta, alpha: a.alpha + b.alpha}; })
-    myBalanceMean.gamma = myBalanceMean.gamma/balanceData.length
-    myBalanceMean.beta = myBalanceMean.beta/balanceData.length
-    myBalanceMean.alpha = myBalanceMean.alpha/balanceData.alpha
 
-    console.log('Balance Mean', myBalanceMean)
+    const {balanceData, reactionData, selfieData} = this.props;
+
+    if(reactionData.length > 0){
+      const myReactionMean = reactionData.reduce((a, b) => { return a + b; })/reactionData.length
+      this.setState({reaction: ((myReactionMean - reactionAvg)/reactionSD)/maxDeviation*100})
+    }
+    
+    if(reactionData.length > 0){
+      const myBalanceMean = balanceData.reduce((a, b) => { return {gamma: a.gamma + b.gamma, beta: a.beta + b.beta, alpha: a.alpha + b.alpha}; })
+      myBalanceMean.gamma = myBalanceMean.gamma/balanceData.length
+      myBalanceMean.beta = myBalanceMean.beta/balanceData.length
+      myBalanceMean.alpha = myBalanceMean.alpha/balanceData.alpha
+      console.log('Balance Mean', myBalanceMean)
+
+    }
+    
+
+    console.log(balanceData, reactionData, selfieData);
 
     // const balMean = this.props.data.reduce((a, b) => { return a + b; })/this.props.data.length
 
