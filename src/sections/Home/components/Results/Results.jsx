9ac7363bd5dd66a4daf8e9f7991f3e4c5cddcef1
NickLewanowicz <nicklewanowicz@gmail.com>
2018-11-03 20:46:31 -0400

    Improve reaction

--- src/sections/Home/components/Results/Results.jsx
+++ src/sections/Home/components/Results/Results.jsx
@@ -2,11 +2,15 @@ import * as React from 'react';
 import './Results.css'
 
 export class Results extends React.Component {
+  state = {
+    reaction: null
+  }
   render() {
     return (
       <div className='stack'>
         <h1>Results!</h1>
         {this.getResults()}
+        {this.state.reaction}
         <div className='instructions'>
         </div>
       </div>
@@ -14,12 +18,14 @@ export class Results extends React.Component {
   }
 
   getResults () {
-    //const reactionAvg = 400
-    //const reactionSD = 40
+    const reactionAvg = 400
+    const reactionSD = 40
+    const maxDeviation = 3
     const {balanceData, reactionData, selfieData} = this.props;
     console.log(balanceData, reactionData, selfieData);
     const myReactionMean = reactionData.reduce((a, b) => { return a + b; })/reactionData.length
-    console.log(myReactionMean, myReactionMean )
+    console.log(myReactionMean, ((myReactionMean - reactionAvg)/reactionSD)/maxDeviation*100)
+    this.setState({reaction: ((myReactionMean - reactionAvg)/reactionSD)/maxDeviation*100})
     // const balMean = this.props.data.reduce((a, b) => { return a + b; })/this.props.data.length
 
     return 0
